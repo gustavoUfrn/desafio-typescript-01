7533ca60043ba610bfc54932e4533a9c4a2daed5
gustavoUfrn <mxgugaa@gmail.com>
2021-07-22 11:16:13 -0300

    Get user by id

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -2,56 +2,57 @@ import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[];
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository;
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = [];
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository();
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    const user = new User();
+        return UsersRepository.INSTANCE;
+    }
 
-    Object.assign(user, {
-      name,
-      email,
-      admin: false,
-      created_at: new Date(),
-      updated_at: new Date(),
-    });
+    create({ name, email }: ICreateUserDTO): User {
+        const user = new User();
 
-    console.log(user);
-    this.users.push(user);
-  }
+        Object.assign(user, {
+            name,
+            email,
+            admin: false,
+            created_at: new Date(),
+            updated_at: new Date(),
+        });
 
-  findById(id: string): User | undefined {
-    const user = this.users.find((user) => user.id === id);
+        console.log(user);
+        this.users.push(user);
+    }
 
-    return user;
-  }
+    findById(id: string): User | undefined {
+        const user = this.users.find((user) => user.id === id);
 
-  findByEmail(email: string): User | undefined {
-    const user = this.users.find((user) => user.email === email);
+        console.log(user);
+        return user;
+    }
 
-    return user;
-  }
+    findByEmail(email: string): User | undefined {
+        const user = this.users.find((user) => user.email === email);
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+        return user;
+    }
 
-  list(): User[] {
-    return this.users;
-  }
+    turnAdmin(receivedUser: User): User {
+        // Complete aqui
+    }
+
+    list(): User[] {
+        return this.users;
+    }
 }
 
 export { UsersRepository };
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -1,1 +1,17 @@
-import { Request, Response } from "express";import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";class ShowUserProfileController {    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}    handle(request: Request, response: Response): Response {        const { user_id } = request.params;        const all = this.showUserProfileUseCase.execute({ user_id });        return response.json(all);    }}export { ShowUserProfileController };
+import { Request, Response } from "express";
+
+import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
+
+class ShowUserProfileController {
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.params;
+
+        const all = this.showUserProfileUseCase.execute({ user_id });
+
+        return response.status(201).json(all);
+    }
+}
+
+export { ShowUserProfileController };
